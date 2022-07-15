63d05211be9c34d2f0078d69bf6aa61bc24be867
NicolasRadic <nicolas.radic05@gmail.com>
2022-07-15 18:37:13 +0200

    start about section

--- src/component/About.js
+++ src/component/About.js
@@ -2,8 +2,24 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="about h-96 bg-red-500">
-      <h1>À propos</h1>
+    <div className="about bg-white  flex flex-col md:flex-row py-5 ">
+      <div className="p-4">
+        <h1 className="p-4 text-2xl font-bold">À propos</h1>
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum,
+          accusantium. Perferendis excepturi eos dolor sed illo quod odio
+          cupiditate illum vel cumque dignissimos nobis consequuntur,
+          consequatur, rem suscipit necessitatibus! Iste voluptatum, adipisci
+          eaque, eum placeat impedit saepe iusto sed quisquam eius cum deleniti
+          blanditiis suscipit quaerat, dicta ipsum. Laboriosam iure temporibus
+          harum possimus architecto quis. Quisquam culpa, ullam commodi dicta ab
+          nihil cupiditate in ducimus dolore reiciendis voluptas eaque
+          asperiores harum hic rerum nulla maiores facere officiis eos aperiam
+          ad? Sapiente, voluptas molestiae eveniet dolorem nostrum non modi nam
+          animi esse ex voluptatibus similique ipsum tempora velit dicta
+          distinctio! Ut?
+        </p>
+      </div>
     </div>
   );
 };
