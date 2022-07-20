cecafcd34f0ad6043355c54fe4b12f310fc36956
NicolasRadic <nicolas.radic05@gmail.com>
2022-07-20 18:07:57 +0200

    start structure of project section

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -2,11 +2,17 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div className="projects flex flex-wrap justify-center items-center p-4 bg-white ">
-      <h1 className="w-1/3 h-96 bg-zinc-300 m-2 ">Projets 1</h1>
-      <h1 className="w-1/3 h-96 bg-zinc-300 m-2 ">Projets 2</h1>
-      <h1 className="w-1/3 h-96 bg-zinc-300 m-2 ">Projets 3</h1>
-      <h1 className="w-1/3 h-96 bg-zinc-300 m-2 ">Projets 4</h1>
+    <div className="projects p-4 flex flex-col justify-center items-center bg-white ">
+      <div>
+        <h2 className="p-4 text-2xl font-bold relative">Projets</h2>
+        <div className="absolute h-2 w-24 bg-cyan translate-x-3.5 -translate-y-4"></div>
+      </div>
+      <div className="flex flex-wrap justify-center items-center mt-4 md:w-10/12">
+        <h3 className="w-[22rem] h-96 bg-zinc-300 m-2 ">Projets 1</h3>
+        <h3 className="w-[22rem] h-96 bg-zinc-300 m-2 ">Projets 2</h3>
+        <h3 className="w-[22rem] h-96 bg-zinc-300 m-2 ">Projets 3</h3>
+        <h3 className="w-[22rem] h-96 bg-zinc-300 m-2 ">Projets 4</h3>
+      </div>
     </div>
   );
 };
